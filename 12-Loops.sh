68d9baa8a353699d1604ef7e0cdcd7ae5114a122
Sanjeetha <sanjeetha.gande@gmail.com>
2026-02-01 18:28:32 +1100

    loops

--- 12-Loops.sh
+++ 12-Loops.sh
@@ -24,7 +24,7 @@ fi
 for package in $@
 do 
 dnf list installed $package &>>LOGS_FILE
-if [ $? -ne 0]; then 
+if [ $? -ne 0 ]; then 
 echo "$package not installed ,installing now"
   dnf install $package -y &>> $LOGS_FILE
   VALIDATE $? "Installing $package"
